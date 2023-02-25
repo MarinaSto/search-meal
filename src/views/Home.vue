1ccfc5518d387da7f297cf48d06b0325da9603f0
Marina Stojmenova <marina.stojmenova@yanovis.com>
2023-02-25 16:19:45 +0100

    Add axios and meal endpoint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,20 @@
         <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search meals">
         <div class="flex gap-2 justify-center mt-2">
             <router-link :to="{name:'byLetter',params:{letter}}" v-for="letter of letters" :key="`letter_${letter}`">
-                {{letter}}
+                {{ letter }}
             </router-link>
         </div>
     </div>
 </template>
 
 <script setup>
-import {computed} from "vue";
-import store from '../store/index.js'
+import {computed, onMounted} from "vue";
+import store from '../store/index.js';
+import axiosClient from "../axiosClient";
 
-const meals = computed(() => store.state.meals)
 const letters = 'ABCDEFGHIJLMNOPQRSTUVWXYZ'
+
+onMounted(async () => {
+    const respons = await axiosClient.get('/list.php?i=list');
+})
 </script>
